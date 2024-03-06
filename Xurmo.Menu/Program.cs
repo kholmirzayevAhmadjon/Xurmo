ebfd39c20cfbcf4b955a5f45703d65e979cf8f9b
Ahmadjon <xolmirzayevahmadjon877@gmail.com>
2024-03-07 01:36:52 +0500

    Add architecture

---
+++ Xurmo.Menu/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Xurmo.Menu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
