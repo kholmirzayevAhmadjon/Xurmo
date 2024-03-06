ebcc83a770a8d88f05d0d5a61aa66dda755426f0
Ahmadjon <xolmirzayevahmadjon877@gmail.com>
2024-03-07 02:00:20 +0500

    Add Auditable

---
+++ Xurmo.Domain/Commons/Auditable.cs
@@ -0,0 +1,14 @@
+﻿namespace Xurmo.Domain.Commons;
+
+public class Auditable
+{
+    public long Id { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public DateTime UpdatedAt { get; set; }
+
+    public DateTime DeletedAt { get; set; }
+
+    public bool IsDeleted { get; set; } = false;
+}
--- Xurmo.Menu/Program.cs
+++ Xurmo.Menu/Program.cs
@@ -1,10 +1,1 @@
-﻿namespace Xurmo.Menu
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿
