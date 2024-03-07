025158ba421c45144756d49d0b44f1cf04b0d9a8
Ahmadjon <xolmirzayevahmadjon877@gmail.com>
2024-03-07 09:07:21 +0500

    Add AddDbContext and Migration

---
+++ Xurmo.Data/AddDbContext/XurmoDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Xurmo.Domain.Categories;
+using Xurmo.Domain.Configuration;
+using Xurmo.Domain.Products;
+
+namespace Xurmo.Data.AddDbContext;
+
+public  class XurmoDbContext : DbContext
+{
+    public DbSet<Category> categories { get; set; }
+    public DbSet<Product> products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(Constants.XURMO_DB);
+    }
+}
---
+++ Xurmo.Domain/Products/Product.cs
@@ -0,0 +1,18 @@
+﻿using Xurmo.Domain.Categories;
+using Xurmo.Domain.Commons;
+
+
+namespace Xurmo.Domain.Products;
+
+public class Product : Auditable
+{
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public decimal Price { get; set; }
+
+    public long CadegoryId { get; set; }
+
+    public Category Categories { get; set; }
+}
