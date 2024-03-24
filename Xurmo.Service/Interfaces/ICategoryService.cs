fa97babf4cf062da4658b141799eb7dd580ad162
Ahmadjon <xolmirzayevahmadjon877@gmail.com>
2024-03-25 01:33:06 +0500

    Added CategoryController

--- Xurmo.Service/Interfaces/ICategoryService.cs
+++ Xurmo.Service/Interfaces/ICategoryService.cs
@@ -5,7 +5,7 @@ namespace Xurmo.Service.Interfaces;
 public interface ICategoryService
 {
     Task<CategoryViewModel> CreateAsync(CategoryCreateModel model);
-    Task<CategoryViewModel> UpdateAsync(long id, CategoryUpdateModel model);
+    Task<CategoryViewModel> UpdateAsync(long id, CategoryUpdateModel model, bool isDelete);
     Task<bool> DeleteAsync(long id);
     Task<CategoryViewModel> GetByIdAsync(long id);
     Task<IEnumerable<CategoryViewModel>> GetAllAsync();
